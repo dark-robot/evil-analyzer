1e35c88aa7c7591b6442fa3ce1a72f1f5596d7c0
Daniel Quiroz <dark.robot.corp@gmail.com>
2015-09-20 12:00:58 -0500

    Add Spring support and application runner

---
+++ evil-analyzer-runner/src/main/java/dark/robot/analyzer/runner/Application.java
@@ -0,0 +1,38 @@
+/**
+ * The evil analyzer tool is a compiler based on Java which is composed of the lexical, syntax and semantic phases.
+ *
+ * Copyright (C) 2015 Dark Robot
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dark.robot.analyzer.runner;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author dark.robot
+ * @version 1.0.0
+ * @since 1.o.0
+ */
+@Configuration
+@ComponentScan
+public class Application {
+
+  public static void main(String[] args) {
+    ApplicationContext context = new AnnotationConfigApplicationContext(Application.class);
+  }
+}
